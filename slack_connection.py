0b436d2c0d08cf1efab2b8f2e685dfe3def432a7
karthikponna <karthikponna963@gmail.com>
2025-10-06 14:11:28 +0530

    add slack testing

---
+++ slack_connection.py
@@ -0,0 +1,29 @@
+import os
+import asyncio
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from dotenv import load_dotenv
+
+load_dotenv()
+
+slack_token = os.getenv("SLACK_BOT_TOKEN")
+client = WebClient(token=slack_token)
+
+if not slack_token:
+    raise RuntimeError("SLACK_BOT_TOKEN not set in environment")
+
+async def send_message():
+    try:
+        response = client.chat_postMessage(
+            channel="C09K6JPAVQ9",
+            text="Hello world"
+        )
+
+        return response
+
+    except SlackApiError as e:
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["error"]  
+
+if __name__=="__main__":
+    asyncio.run(send_message())
