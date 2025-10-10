6bc8a6d4568043a32883a95a759a4732d09522de
karthikponna <karthikponna963@gmail.com>
2025-10-10 16:36:24 +0530

    docs: Testing some code

--- apps/slack-integrations-offline/testing.py
+++ apps/slack-integrations-offline/testing.py
@@ -1,12 +1,39 @@
 import asyncio
 from crawl4ai import AsyncWebCrawler
+from loguru import logger
 
-async def main():
+
+async def main(url):
     # Create an instance of AsyncWebCrawler
     async with AsyncWebCrawler() as crawler:
         # Run the crawler on a URL
-        result = await crawler.arun(url="https://docs.clearfeed.ai/clearfeed-help-center/answers/ai-agents")
-        
+        result = await crawler.arun(url= url)
+        print(result.success)
+        print("*" * 50)
+
+        if not result or not result.success:
+            logger.warning(f"Failed to crawl {url}")
+            
+            return None
+
+        if result.markdown is None:
+            logger.warning(f"Failed to crawl {url}")
+            return None
+
+        child_links = [
+            link["href"]
+            for link in result.links["internal"] + result.links["external"]
+        ]
+
+        print(child_links)
+        print("-"*50)
+        print(result.metadata)
+
+        if result.metadata:
+            title = result.metadata.pop("title", "") or ""
+        else:
+            title = ""
+
         
         if result:
             with open("data/clearfeed_crawled_documents.md", "w") as file:
@@ -14,4 +41,4 @@ async def main():
 
 if __name__=="__main__":
     # Run the async main function
-    asyncio.run(main())
+    asyncio.run(main(url = "https://docs.clearfeed.ai/clearfeed-help-center/answers/ai-agents"))
