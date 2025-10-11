4a6a64901bd5f78fbda043ee69b9fa3623637f86
karthikponna <karthikponna963@gmail.com>
2025-10-11 16:46:40 +0530

    Add document pydantic class

---
+++ apps/slack-integrations-offline/src/slack_integrations_offline/domain/document.py
@@ -0,0 +1,57 @@
+import json
+
+from pathlib import Path
+from pydantic import BaseModel, Field
+
+from slack_integrations_offline.utils import generate_random_hex
+
+
+class DocumentMetadata(BaseModel):
+    id: str
+    url: str
+    title: str
+    properties: dict
+
+
+class Document(BaseModel):
+    id: str = Field(default_factory=lambda: generate_random_hex(length=32))
+    metadata: DocumentMetadata
+    content: str
+    content_quality_score: float | None = None
+    child_urls: list[str] = Field(default_factory=list)
+
+
+    def write(
+        self, output_dir: Path, also_save_as_txt: bool = False,
+    ) -> None:
+        
+        json_page = self.model_dump()
+
+        output_file = output_dir/f"{self.id}.json"
+
+        with open(output_file, "w", encoding="utf-8") as f:
+            json.dump(
+                json_page,
+                f,
+                indent=4,
+                ensure_ascii=False,
+            )
+
+        if also_save_as_txt:
+            txt_path = output_file.with_suffix(".txt")
+            with open(txt_path, "w", encoding="utf-8") as f:
+                f.write(self.content)
+
+        
+
+
+    def __eq__(self, other: object) -> bool:
+        
+        if not isinstance(other, Document):
+            return False
+        return self.id == other.id
+    
+
+    def __hash__(self) -> int:
+
+        return hash(self.id)
