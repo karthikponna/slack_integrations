ac0378d808af0f54be1b3082f947da196746122d
karthikponna <karthikponna963@gmail.com>
2025-10-14 14:33:48 +0530

    docs: Update crawl4ai pipeline completed

--- apps/slack-integrations-offline/pipelines/collect_crawl_data.py
+++ apps/slack-integrations-offline/pipelines/collect_crawl_data.py
@@ -3,7 +3,29 @@ from pathlib import Path
 from loguru import logger
 from zenml import pipeline
 
+from steps.collect_crawl_data.extract_crawled_data import extract_crawled_data
+from steps.infrastructure.save_documents_to_disk import save_documents_to_disk
+from steps.infrastructure.upload_to_s3 import upload_to_s3
 
 @pipeline
-def collect_crawl_data():
-    pass
+def collect_crawl_data(
+    urls:list[str] = [], 
+    data_dir: Path = Path(),
+    to_s3: bool = False,
+    max_workers:int = 10,
+) -> None:
+
+    crawled_data_dir = data_dir / "crawled"
+    logger.info(f"Saving crawled data to {crawled_data_dir}")
+    
+    crawled_documents = extract_crawled_data(urls=urls, max_workers=max_workers)
+
+    save_documents_to_disk(documents=crawled_documents, output_dir=crawled_data_dir)
+
+    if to_s3:
+        upload_to_s3(
+        folder_path=str(crawled_data_dir),
+        s3_prefix="slack_integrations/crawled",
+        after="save_documents_to_disk"
+        )
+
