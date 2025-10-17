be3cb264f8e24c9d679b0edf3a3b6d278f189fde
karthikponna <karthikponna963@gmail.com>
2025-10-17 13:36:44 +0530

    docs: Add upload to s3 code

--- apps/slack-integrations-offline/steps/infrastructure/upload_to_s3.py
+++ apps/slack-integrations-offline/steps/infrastructure/upload_to_s3.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import time
+from loguru import logger
 
 from typing_extensions import Annotated
 from zenml import get_step_context, step
@@ -13,8 +15,12 @@ def upload_to_s3(
     s3_prefix: str = "",
 ) -> Annotated[str, "output"]:
     
+    time.sleep(1)
+
     s3_client = S3Client(bucket_name=settings.AWS_S3_BUCKET_NAME)
-    s3_client.upload_folder(local_path=folder_path, s3_prefix=s3_prefix)
+    s3_key = s3_client.upload_folder(local_path=folder_path, s3_prefix=s3_prefix)
+
+    download_url = s3_client.generate_presigned_url(s3_key=s3_key)
     
     step_context = get_step_context()
     step_context.add_output_metadata(
@@ -22,7 +28,10 @@ def upload_to_s3(
         metadata={
             "folder_path": str(folder_path),
             "s3_prefix": s3_prefix,
+            "download_url": download_url,
         }
     )
 
+    logger.info(f"Uploaded to S3: {s3_key}")
+    logger.info(f"Download URL: {download_url}")
     return str(folder_path)
