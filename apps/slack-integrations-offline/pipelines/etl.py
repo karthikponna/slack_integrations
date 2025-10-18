f8d8ea114412efab1ce9e38a42e42c3860ffebef
karthikponna <karthikponna963@gmail.com>
2025-10-18 19:19:25 +0530

    docs: Update etl in summarizing docs and saving it to local data dir

--- apps/slack-integrations-offline/pipelines/etl.py
+++ apps/slack-integrations-offline/pipelines/etl.py
@@ -4,15 +4,39 @@ from loguru import logger
 from zenml import pipeline
 
 from steps.infrastructure.read_documents_from_disk import read_documents_from_disk
+from steps.generate_summaries.generate_summary import generate_summary
+from steps.infrastructure.save_documents_to_disk import save_documents_to_disk
 
+from src.slack_integrations_offline.domain.document import Document
 
 @pipeline
 def etl(
+    summarization_model: str,
+    documents: list[Document],
     data_dir: Path = Path(),
+    temperature: float = 0.0,
+    max_workers: int = 10,
+    min_document_length: int = 50,
+    summarization_max_characters: int = 1000,
 ) -> None:
     
     crawled_data_dir = data_dir / "crawled"
 
+    enhanced_data_dir = data_dir / "enhanced"
+
     documents = read_documents_from_disk(
         data_directory = crawled_data_dir, nesting_level = 0
     )
+
+    enhanced_documents = generate_summary(
+        summarization_model=summarization_model,
+        documents=documents,
+        temperature=temperature,
+        max_workers=max_workers,
+        min_document_length=min_document_length,
+        summarization_max_characters=summarization_max_characters,
+    )
+
+    save_documents_to_disk(documents=enhanced_documents, output_dir=enhanced_data_dir)
+    
+    
