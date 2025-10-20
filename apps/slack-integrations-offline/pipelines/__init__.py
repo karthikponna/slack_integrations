a6981f67e70b98ec07527a94a3b3c09519940cea
karthikponna <karthikponna963@gmail.com>
2025-10-20 07:00:21 +0530

    updates

--- apps/slack-integrations-offline/pipelines/__init__.py
+++ apps/slack-integrations-offline/pipelines/__init__.py
@@ -1,8 +1,10 @@
 from .collect_crawl_data import collect_crawl_data
+from .etl import etl
 from .testing_pipeline import testing_pipeline
 
 __all__ = [
     "collect_crawl_data",
+    "etl",
     "testing_pipeline",
 
 ]
