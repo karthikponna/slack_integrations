c1e76089df95c13d7f6a5f86886cd5289c09ba5a
karthikponna <karthikponna963@gmail.com>
2025-10-24 19:59:01 +0530

    docs: Add ingest to mongodb

--- apps/slack-integrations-offline/pipelines/etl.py
+++ apps/slack-integrations-offline/pipelines/etl.py
@@ -6,17 +6,17 @@ from zenml import pipeline
 from steps.infrastructure.read_documents_from_disk import read_documents_from_disk
 from steps.generate_summaries.generate_summary import generate_summary
 from steps.infrastructure.save_documents_to_disk import save_documents_to_disk
+from steps.infrastructure.ingest_to_mongodb import ingest_to_mongodb
 
 from src.slack_integrations_offline.domain.document import Document
 
 @pipeline
 def etl(
     summarization_model: str,
-    documents: list[Document],
+    load_collection_name: str,
     data_dir: Path = Path(),
     temperature: float = 0.0,
     max_workers: int = 10,
-    min_document_length: int = 50,
     summarization_max_characters: int = 1000,
 ) -> None:
     
@@ -33,10 +33,14 @@ def etl(
         documents=documents,
         temperature=temperature,
         max_workers=max_workers,
-        min_document_length=min_document_length,
         summarization_max_characters=summarization_max_characters,
     )
 
     save_documents_to_disk(documents=enhanced_documents, output_dir=enhanced_data_dir)
     
-    
+
+    ingest_to_mongodb(
+        models=enhanced_documents,
+        collection_name=load_collection_name,
+        clear_collection=True
+    )
