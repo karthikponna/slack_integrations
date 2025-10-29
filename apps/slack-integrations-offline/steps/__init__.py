e32a5c9c35651c7ce2cc6401981f5bbad1ebea4d
karthikponna <karthikponna963@gmail.com>
2025-10-29 13:54:01 +0530

    Update init file of steps folder

--- apps/slack-integrations-offline/steps/__init__.py
+++ apps/slack-integrations-offline/steps/__init__.py
@@ -1,3 +1,9 @@
-from . import testing_step
+from . import collect_crawl_data, collect_urls, compute_rag, generate_summaries, infrastructure
 
-__all__ = ["testing_step",]
+__all__ = [
+    "collect_crawl_data",
+    "collect_urls",
+    "compute_rag",
+    "generate_summaries",
+    "infrastructure"
+]
