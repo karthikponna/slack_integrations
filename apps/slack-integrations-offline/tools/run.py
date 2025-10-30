49517daa951f37596c91e8adf5cc80c2b2727f2e
karthikponna <karthikponna963@gmail.com>
2025-10-30 20:07:18 +0530

    docs: Update compute rag cli command

--- apps/slack-integrations-offline/tools/run.py
+++ apps/slack-integrations-offline/tools/run.py
@@ -7,6 +7,7 @@ import click
 from pipelines import (
     collect_crawl_data,
     etl,
+    compute_rag,
 )
 
 
@@ -26,9 +27,16 @@ from pipelines import (
     default=False,
     help="Whether to run the etl pipeline."
 )
+@click.option(
+    "--run-compute-rag-pipeline",
+    is_flag=True,
+    default=False,
+    help="Whether to run the compute rag pipeline."
+)
 def main(
     run_collect_crawl_data_pipeline: bool = False,
     run_etl_pipeline: bool = False,
+    run_compute_rag_pipeline: bool = False,
 ) -> None:
     
     pipeline_args: dict[str, Any] = {
@@ -48,6 +56,7 @@ def main(
         )
         collect_crawl_data.with_options(**pipeline_args)(**run_args)
 
+
     if run_etl_pipeline:
         run_args = {}
         pipeline_args["config_path"] = root_dir / "configs" / "etl.yaml"
@@ -56,6 +65,16 @@ def main(
         )
         pipeline_args["run_name"] = f"etl_pipeline_run_{dt.now().strftime('%Y_%m_%d_%H_%M_%S')}"
         etl.with_options(**pipeline_args)(**run_args)
+
+
+    if run_compute_rag_pipeline:
+        run_args = {}
+        pipeline_args["config_path"] = root_dir / "configs" / "compute_rag.yaml"
+        assert pipeline_args["config_path"].exists(), (
+            f"Config file not found: {pipeline_args['config_path']}"
+        )
+        pipeline_args["run_name"] = f"compute_rag_pipeline_run_{dt.now().strftime('%Y_%m_%d_%H_%M_%S')}"
+        compute_rag.with_options(**pipeline_args)(**run_args)
 
 
 if __name__ == "__main__":
