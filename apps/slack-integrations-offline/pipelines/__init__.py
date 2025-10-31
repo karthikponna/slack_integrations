6752f05ba20939c0ce65b3b612487f656bb93fe9
karthikponna <karthikponna963@gmail.com>
2025-10-31 20:17:13 +0530

    docs: Update pipeline imports init file

--- apps/slack-integrations-offline/pipelines/__init__.py
+++ apps/slack-integrations-offline/pipelines/__init__.py
@@ -1,11 +1,8 @@
 from .collect_crawl_data import collect_crawl_data
 from .etl import etl
 from .compute_rag import compute_rag
-from .testing_pipeline import testing_pipeline
 
 __all__ = [
     "collect_crawl_data",
     "etl",
-    "testing_pipeline",
-
 ]
