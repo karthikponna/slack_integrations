b22cdfcc3ca7f2ad99f07f2582a5caee256210e8
karthikponna <karthikponna963@gmail.com>
2025-11-18 16:18:38 +0530

    Updates

--- apps/slack-integrations-offline/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from slack-integrations-offline!")
-
-
-if __name__ == "__main__":
-    main()
--- apps/slack-integrations-online/src/slack_integrations_online/config.py
+++ apps/slack-integrations-online/src/slack_integrations_online/config.py
@@ -29,7 +29,8 @@ class Settings(BaseSettings):
     )
 
     MONGODB_URI: str = Field(
-        default="mongodb+srv://karthikponna963_db_user:ccWG3CrTZbWiEhXd@cluster0.yuyvgml.mongodb.net/",
+        default="mongodb://slack:slack@localhost:27017/?directConnection=true", 
+        # default="mongodb+srv://karthikponna963_db_user:ccWG3CrTZbWiEhXd@cluster0.yuyvgml.mongodb.net/",
         description="Connection URI for the local MongoDB Atlas instance.",
     )
 
